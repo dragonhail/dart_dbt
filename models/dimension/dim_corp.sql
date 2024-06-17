d2039cae9f154e667edb1c07f6dd22006f8c9d05
dragonhail <dragonhailstone@gmail.com>
2024-06-17 17:19:32 +0900

    add folder

---
+++ models/dimension/dim_corp.sql
@@ -0,0 +1,9 @@
+-- Dimension 테이블 생성: 회사 정보
+with dim_company as (
+    SELECT
+    company_id SERIAL PRIMARY KEY,
+    corp_code VARCHAR(20) UNIQUE,
+    corp_name VARCHAR(100)
+    FROM
+    ref{{ }}
+);
